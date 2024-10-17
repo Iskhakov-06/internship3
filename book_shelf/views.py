2b1cccbd053a6b5108380c38c8ccb526b1ff364b
Iskander <iskandelive@gmail.com>
2024-10-17 19:49:19 +0500

    Сделал дз №5

--- book_shelf/views.py
+++ book_shelf/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'book_shelf/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'book_shelf/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'price', 'manufacturer', 'warehouse', 'image']
+    template_name = 'book_shelf/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'price', 'manufacturer', 'warehouse', 'image']
+    template_name = 'book_shelf/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'book_shelf/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
--- dj_project/urls.py
+++ dj_project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book_shelf.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('admin/', admin.site.urls),
 ]
+
